55581754841feebd3a368e806e2b91caefabba0d
sondosakramm <54165079+sondosakramm@users.noreply.github.com>
2021-08-02 13:28:34 +0200

    Update Image Analysi.py

--- Image Analysi.py
+++ Image Analysi.py
@@ -1,14 +1,15 @@
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-
 from msrest.authentication import CognitiveServicesCredentials
-
 
 ENDPOINT = "https://image-analyzer1.cognitiveservices.azure.com/"
 KEY = "3f8fb94c6c804e02b661a15997a76809"
-IMAGE_URL = "C:\Users\SONDOS\Desktop\photo-1626296051075-13a2c43258f1.webp"
+
+image_url1 = "https://i.ibb.co/c8v6JP5/image.jpg"
+image_url2 = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
 
 client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-description = client.describe_image(IMAGE_URL)
+description = client.describe_image(image_url1)
 
 for caption in description.captions:
   print(caption.text)
+
